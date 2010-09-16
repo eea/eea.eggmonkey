f3c8faeb862b8831031ccf9a10effa01456d9ad8
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-09-16 12:36:46 +0000

    Tagged eea.eggmonkey 0.6.

---
+++ trunk/eea.eggmonkey/tags/0.6/setup.py
@@ -0,0 +1,46 @@
+import os
+from os.path import join
+from setuptools import setup, find_packages
+
+def read(*pathnames):
+    return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
+
+name = 'eea.eggmonkey'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
+
+setup(name=name,
+        version=version,
+        description="Automate releasing eggs with jarn.mkrelease",
+        long_description='\n'.join([
+            read('docs', 'README.txt'),
+            read('docs', 'HISTORY.txt'),
+            ]),
+        classifiers=[
+            "Framework :: buildout",
+            "Programming Language :: Python",
+            ],
+        keywords='buildout',
+        author='Tiberiu Ichim',
+        author_email='tiberiu@eaudeweb.ro',
+        url='https://svn.eionet.europa.eu/repositories/Zope/trunk/eea.eggmonkey',
+        license='GPL',
+        packages=find_packages(),
+        namespace_packages=['eea'],
+        include_package_data=True,
+        zip_safe=False,
+
+        install_requires=[
+            'setuptools',
+            'argparse',
+            'mr.developer',
+            ],
+        entry_points={
+            "console_scripts":[
+                "monkey = eea.eggmonkey.monkey:main" ,
+                ],
+            "zc.buildout.unloadextension":[
+                "monkey = eea.eggmonkey.buildout:learn",
+                ]
+            }
+        )
